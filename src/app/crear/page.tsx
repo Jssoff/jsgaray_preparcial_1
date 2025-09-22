1f401949b1f13a554d03c7d27a535bd979a29a97
Jssoff <js.garay@uniandes.com>
2025-09-21 20:51:00 -0500

    actualizacion ruta

--- src/app/crear/page.tsx
+++ src/app/crear/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AuthorForm } from "@/app/crear/services/authorForm";
+import { AuthorForm } from "@/app/crear/Components/authorForm";
 
 export default function CreateAuthorPage() {
   return (
